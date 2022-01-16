66aea9479b84395f3fdb578aa3a6214fffe3aff4
Raissa <ra.sanf@gmail.com>
2022-01-15 21:51:52 -0300

    feat: added accordion list

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
-tabContent[0].classList.add('ativo');
+// Função de navegação por tabs da seção ANIMAIS
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-if(tabMenu.length && tabContent.length) {
-    function activeTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove('ativo');
+    if(tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('ativo');
+
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+            tabContent[index].classList.add('ativo');
+        }
+        
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
         });
-        tabContent[index].classList.add('ativo');
     }
-    
-    tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-            activeTab(index);
-        });
-    });
 }
+initTabNav();
+
+// Função accordion das perguntas da seção FAQ
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClass = 'ativo';
+
+    if(accordionList.length) {
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass);
+
+        function activeAccordion() {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion); 
+        })
+    }
+}
+initAccordion();
